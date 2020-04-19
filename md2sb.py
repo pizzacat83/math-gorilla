322bc2bc5fe6b764452f20aa50573c1aaac7c215
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-04-19 22:38:24 +0900

    add files

---
+++ md2sb.py
@@ -0,0 +1,19 @@
+import re
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file", help="path to the Markdown file")
+args = parser.parse_args()
+
+s=None
+if args.file is None:
+    s=r"""
+"""
+else:
+    f = open(args.file)
+    s = f.read()
+
+r = re.compile(r"\$(.+?)\$",)
+r2 = re.compile(r"\[([!\"#%&'()*+,-./{|}<>_~]+ .+)$") # not used
+
+print('\n'.join([r.sub(r'[$ \1 ]', l) for l in s.split("\n")]))
